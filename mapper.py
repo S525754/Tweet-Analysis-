b7ea1a92239b75f0f1a1b62422e83a7869d15b77
Vinod Kumar Reddy Peram <S525754@mail.nwmissouri.edu>
2017-04-10 16:31:33 -0500

    Added comments regarding input output files

--- mapper.py
+++ mapper.py
@@ -4,9 +4,8 @@ for line in f:
     data = line.strip().split(",") # Split the input data based on ',' seperated
     print data
     print len(data)
-    if len(data) == 15:
-        tweet_id, airline_sentiment, airline_sentiment_confidence, negativereason, negativereason_confidence, airline, airline_sentiment_gold, name, negativereason_gold, retweet_count, text, tweet_coord, tweet_created, tweet_location, user_timezone = data  #assign each entry to a variable
-        print "{0}\t{1}".format(airline, airline_sentiment)
+    if len(data) == 15: # assign data
+        tweet_id, airline_sentiment, airline_sentiment_confidence, negativereason, negativereason_confidence, airline, airline_sentiment_gold, name, negativereason_gold, retweet_count, text, tweet_coord, tweet_created, tweet_location, user_timezone = data  
         o.write("{0}\t{1}\n".format(airline, airline_sentiment))
-f.close()
-o.close()
+f.close() # Closing file1
+o.close() # CLosing outputfile
