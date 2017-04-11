82f02105910d2faba65aa3f33afa0ce8186c6314
anil2648 <s525810@mail.nwmissouri.edu>
2017-04-11 16:05:09 -0500

    Added changes to first mapper and reducer code. It is working fine now

--- mapper.py
+++ mapper.py
@@ -1,11 +1,11 @@
-f = open("Tweets.csv","r")  # open file, read-only
-o = open("mOutput.txt", "w") # open file, write-only
+f = open("Tweets.csv","r")  
+o = open("moutput.txt", "w") 
 for line in f:  
-    data = line.strip().split(",") # Split the input data based on ',' seperated
-    print data
-    print len(data)
-    if len(data) == 15: # assign data
-        tweet_id, airline_sentiment, airline_sentiment_confidence, negativereason, negativereason_confidence, airline, airline_sentiment_gold, name, negativereason_gold, retweet_count, text, tweet_coord, tweet_created, tweet_location, user_timezone = data  
-        o.write("{0}\t{1}\n".format(airline, airline_sentiment))
-f.close() # Closing file1
-o.close() # CLosing outputfile
+    data = line.strip().split(",") 
+      
+    if len(data) == 15:
+        tweet_id, airline_sentiment, airline_sentiment_confidence, negativereason, negativereason_confidence, airline, airline_sentiment_gold, name, negativereason_gold, retweet_count, text, tweet_coord, tweet_created, tweet_location, user_timezone = data
+        print "{0}\t{1}".format(airline, airline_sentiment)
+        o.write("{0},{1}\n".format(airline, airline_sentiment))
+f.close()
+o.close()
--- mapper2.py
+++ mapper2.py
@@ -1,9 +1,13 @@
-f = open("d.txt","r")           # open file, read-only
+f = open("Tweets.csv","r")  
+o = open("moutput2.txt", "w") 
 for line in f:  
-    data = line.strip().split("    ") 
+    data = line.strip().split(",") 
     print data
     print len(data)
-    if len(data) == 6:
-        date, time, store, item, cost, payment = data  #assign each entry to a variable
-        print "{0}\t{1}".format(store, cost)
+    if len(data) == 15:
+		value = 1
+        tweet_id, airline_sentiment, airline_sentiment_confidence, negativereason, negativereason_confidence, airline, airline_sentiment_gold, name, negativereason_gold, retweet_count, text, tweet_coord, tweet_created, tweet_location, user_timezone = data
+        print "{0}\t{1}".format(negativereason, value)
+        o.write("{0}\t{1}\n".format(negativereason, value))
 f.close()
+o.close()
